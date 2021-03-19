1e12c8a34276cb9d70eb960b141d0651810cc0e0
antoni perez <antoni.protoni@gmail.com>
2021-03-19 14:41:02 -0300

    pimer fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import ListaTareas from './components/ListaTareas';
+import Tarea from "./components/Tarea"
+
+
+
+
+
 
 function App() {
+
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+
+
+  useEffect(() => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/yoka")
+    .then(r => r.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>TO DO LIST </h1>
+    <form 
+      onSubmit={event => { 
+      event.preventDefault();
+      
+      setTareas(tareas.concat(tarea));
+      console.log(tareas);
+      setTarea('');
+
+
+    }} >
+      <input type="text" 
+             placeholder ="ingresa una tarea" 
+             onChange= {event => setTarea(event.target.value) }
+             value = {tarea}
+             
+             
+             />
+              
+    </form>
+    <p>{tarea}</p>
+    <ul>
+      {tareas.map((item,index)=> {
+      return <li key={index}>{item} <span 
+      style ={{cursor:'pointer'}}
+      onClick= {(e) => {
+        setTareas(
+          tareas.filter((elemento,i) => {
+            if (index != i ) {
+              return elemento;
+            }
+
+          }
+
+          )
+        )
+
+      }}>X</span></li>;
+      })}
+      
+    </ul>
+    <ListaTareas/>
+    <Tarea/>
     </div>
   );
 }
---
+++ src/components/ListaTareas.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ListaTareas() {
+    return (
+        <div>
+            lista tareas
+        </div>
+    )
+}
